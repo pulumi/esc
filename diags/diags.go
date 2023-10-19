457cdce26e08f860ce6df2770b67baea259fc571
Levi Blackstone <levi@pulumi.com>
2023-10-19 10:16:03 -0600

    Clean up typo and ordering

--- diags/diags.go
+++ diags/diags.go
@@ -21,35 +21,25 @@ import (
 )
 
 // A formatter for when a field or property is used that does not exist.
-type NonExistantFieldFormatter struct {
+type NonExistentFieldFormatter struct {
 	ParentLabel         string
 	Fields              []string
 	MaxElements         int
 	FieldsAreProperties bool
 }
 
-func (e NonExistantFieldFormatter) fieldsName() string {
+func (e NonExistentFieldFormatter) fieldsName() string {
 	if e.FieldsAreProperties {
 		return "properties"
 	}
 	return "fields"
 }
 
-// Get a single line message.
-func (e NonExistantFieldFormatter) Message(field, fieldLabel string) string {
-	return fmt.Sprintf("%s %s", e.messageHeader(fieldLabel), e.messageBody(field))
-}
-
-// A message broken up into a top level and detail line
-func (e NonExistantFieldFormatter) MessageWithDetail(field, fieldLabel string) (string, string) {
-	return e.messageHeader(fieldLabel), e.messageBody(field)
-}
-
-func (e NonExistantFieldFormatter) messageHeader(fieldLabel string) string {
+func (e NonExistentFieldFormatter) messageHeader(fieldLabel string) string {
 	return fmt.Sprintf("%s does not exist on %s", fieldLabel, e.ParentLabel)
 }
 
-func (e NonExistantFieldFormatter) messageBody(field string) string {
+func (e NonExistentFieldFormatter) messageBody(field string) string {
 	existing := sortByEditDistance(e.Fields, field)
 	if len(existing) == 0 {
 		return fmt.Sprintf("%s has no %s", e.ParentLabel, e.fieldsName())
@@ -59,6 +49,16 @@ func (e NonExistantFieldFormatter) messageBody(field string) string {
 		list = fmt.Sprintf("%s and %d others", strings.Join(existing[:5], ", "), len(existing)-e.MaxElements)
 	}
 	return fmt.Sprintf("Existing %s are: %s", e.fieldsName(), list)
+}
+
+// Get a single line message.
+func (e NonExistentFieldFormatter) Message(field, fieldLabel string) string {
+	return fmt.Sprintf("%s %s", e.messageHeader(fieldLabel), e.messageBody(field))
+}
+
+// A message broken up into a top level and detail line
+func (e NonExistentFieldFormatter) MessageWithDetail(field, fieldLabel string) (string, string) {
+	return e.messageHeader(fieldLabel), e.messageBody(field)
 }
 
 // A formatter for missing fields that may be valid in other places.
