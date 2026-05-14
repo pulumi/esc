eb4dc960eb82a290200de86be78ee839829f5ab0
Boris Schlosser <boris@pulumi.com>
2026-05-14 12:50:36 +0200

    esc env provider: scaffold parent command

---
+++ cmd/esc/cli/env_provider.go
@@ -0,0 +1,23 @@
+// Copyright 2026, Pulumi Corporation.
+
+package cli
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func newEnvProviderCmd(env *envCommand) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "provider",
+		Short: "Manage login providers within an environment",
+		Long: "Manage login providers within an environment\n" +
+			"\n" +
+			"Subcommands add cloud-provider login blocks (AWS, Azure, GCP) to an environment.\n" +
+			"Only static credentials are supported today; OIDC support will be added later.\n",
+		Args: cobra.NoArgs,
+	}
+
+	cmd.AddCommand(newEnvProviderAWSLoginCmd(env))
+
+	return cmd
+}
